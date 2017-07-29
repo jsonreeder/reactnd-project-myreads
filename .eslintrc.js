df889129c1d66ed5bbd05416bed6db1a8ec3277b
Jason Reeder <info@jasonreeder.com>
2017-07-29 13:37:39 -0700

    style: Add style enforcement

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: 'babel-eslint',
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'max-len': ['error', 100],
+    'no-undef': 'error',
+    'no-unused-vars': 'error',
+    'no-unused-expressions': 'error',
+    'no-trailing-spaces': 'error',
+    'sort-imports': ['error'],
+    'sort-keys': ['error', 'asc'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: 'error',
+  }
+}
