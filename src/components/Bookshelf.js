7b281fe3a392f3249543ce4f4ba1577d12a06938
Jason Reeder <info@jasonreeder.com>
2017-08-07 21:02:27 -0700

    feat: Improve shelf names

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,8 +1,15 @@
 import { Book } from './Book';
 import React from 'react';
 
+const titles = {
+  currentlyReading: 'Currently Reading',
+  read: 'Read',
+  wantToRead: 'Want To Read',
+};
+
 export const Bookshelf = (props) => {
   const { books, title } = props;
+  const shelfTitle = titles[title];
   const booksGrid = (
     <ol className="books-grid">
       {books.map((b, i) => <li key={i}><Book authors={b.authors} title={b.title} image={b.image} /></li>)}
@@ -11,7 +18,7 @@ export const Bookshelf = (props) => {
 
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">{title}</h2>
+      <h2 className="bookshelf-title">{shelfTitle}</h2>
       <div className="bookshelf-books">
         {booksGrid}
       </div>
